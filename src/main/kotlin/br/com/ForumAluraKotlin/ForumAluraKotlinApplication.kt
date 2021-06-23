75b163c3c0ef3ae147dd76bb4e1e7b905ab7a7c1
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-23 11:41:30 -0300

    setup

---
+++ src/main/kotlin/br/com/ForumAluraKotlin/ForumAluraKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ForumAluraKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumAluraKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumAluraKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/ForumAluraKotlin/ForumAluraKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ForumAluraKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ForumAluraKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
